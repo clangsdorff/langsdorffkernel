5ffc78e5384ad267cf73283c3355960b3d353dee
Langsdorff <131063968+clangsdorff@users.noreply.github.com>
2026-07-09 19:54:25 +0300

    update binary D again

--- net/mac80211/tx.c
+++ net/mac80211/tx.c
@@ -2027,9 +2027,7 @@ void ieee80211_xmit(struct ieee80211_sub_if_data *sdata,
 		}
 	}
 
-	if (likely(info->control.vif->type != NL80211_IFTYPE_MONITOR)) {
-		ieee80211_set_qos_hdr(sdata, skb);
-	}
+	ieee80211_set_qos_hdr(sdata, skb);
 	ieee80211_tx(sdata, sta, skb, false);
 }
 
