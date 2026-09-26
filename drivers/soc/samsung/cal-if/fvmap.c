a71b1f06d9210121aebcca8aa1f687ce3b2ee5b1
Langsdorff <131063968+clangsdorff@users.noreply.github.com>
2026-09-26 03:53:57 +0300

    soc: samsung: cal-if: fvmap: expose ACPM CPU level 0 PLL and voltage for OC probing

--- drivers/soc/samsung/cal-if/fvmap.c
+++ drivers/soc/samsung/cal-if/fvmap.c
@@ -8,6 +8,7 @@
 #include <soc/samsung/cal-if.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
+#include <linux/mutex.h>
 #include <soc/samsung/fvmap.h>
 
 #include "cmucal.h"
@@ -341,6 +342,279 @@ static void fvmap_copy_from_sram(void __iomem *map_base, void __iomem *sram_base
 	vfree((void *)header);
 }
 
+#define OC_PLL_M_STOCK		255
+#define OC_PLL_M_MAX		276
+#define OC_VOLT_MAX_UV		1350000
+#define OC_VOLT_MIN_UV		600000
+#define OC_MAX_LEVEL		32
+
+struct oc_cluster {
+	int idx;
+	struct cmucal_pll *pll;
+	void __iomem *pll_hdr;
+	void __iomem *pms0;
+	void __iomem *volt0;
+	u32 orig_pms0;
+	u32 orig_volt0;
+	bool volt_steps;
+};
+
+static struct oc_cluster oc_cl[2] = { { .idx = -1 }, { .idx = -1 } };
+static DEFINE_MUTEX(oc_lock);
+
+static u32 oc_field(u32 v, unsigned char shift, unsigned char width)
+{
+	return (v >> shift) & width_to_mask(width);
+}
+
+static bool oc_pms_is_stock(struct oc_cluster *c)
+{
+	struct cmucal_pll *pll = c->pll;
+
+	if (!pll || !pll->m_width)
+		return false;
+
+	return oc_field(c->orig_pms0, pll->m_shift, pll->m_width) == OC_PLL_M_STOCK &&
+	       oc_field(c->orig_pms0, pll->p_shift, pll->p_width) == 3 &&
+	       oc_field(c->orig_pms0, pll->s_shift, pll->s_width) == 0;
+}
+
+static unsigned int oc_volt_to_uv(struct oc_cluster *c, u32 raw)
+{
+	return c->volt_steps ? raw * STEP_UV : raw;
+}
+
+static void cpu_oc_setup(void __iomem *sram_base)
+{
+	struct fvmap_header *fh = fvmap_base;
+	int size = cmucal_get_list_size(ACPM_VCLK_TYPE);
+	struct cmucal_clk *clk;
+	struct oc_cluster *c;
+	struct vclk *vclk;
+	u16 hdr_off, level;
+	u8 lv0_idx;
+	int i;
+
+	for (i = 0; i < size; i++) {
+		vclk = cmucal_get_node(ACPM_VCLK_TYPE | i);
+		if (!vclk || (vclk->margin_id != MARGIN_CPUCL0 && vclk->margin_id != MARGIN_CPUCL1))
+			continue;
+		if (!fh[i].num_of_pll || !vclk->num_list || fh[i].o_members & 1 ||
+		    fh[i].o_members + 2 > FVMAP_SIZE)
+			continue;
+
+		hdr_off = readw(sram_base + fh[i].o_members);
+		if ((hdr_off | fh[i].o_ratevolt) & 3 || hdr_off + 8 > FVMAP_SIZE)
+			continue;
+
+		level = readw(sram_base + hdr_off + 6);
+		if (!level || level > OC_MAX_LEVEL ||
+		    hdr_off + 8 + level * 4 > FVMAP_SIZE ||
+		    fh[i].o_ratevolt + fh[i].num_of_lv * 8 > FVMAP_SIZE ||
+		    fh[i].o_tables + fh[i].num_of_lv * fh[i].num_of_members > FVMAP_SIZE)
+			continue;
+
+		c = &oc_cl[vclk->margin_id - MARGIN_CPUCL0];
+		if (c->idx >= 0)
+			continue;
+		c->idx = i;
+		c->pll_hdr = sram_base + hdr_off;
+		lv0_idx = readb(sram_base + fh[i].o_tables);
+		if (lv0_idx < level)
+			c->pms0 = c->pll_hdr + 8 + lv0_idx * 4;
+
+		c->volt0 = sram_base + fh[i].o_ratevolt + 4;
+		c->orig_volt0 = readl(c->volt0);
+		c->volt_steps = c->orig_volt0 < 1000;
+
+		clk = cmucal_get_node(vclk->list[0]);
+		if (clk && IS_PLL(clk->id))
+			c->pll = to_pll_clk(clk);
+
+		if (c->pms0)
+			c->orig_pms0 = readl(c->pms0);
+
+		pr_info("cpu_oc: %s idx %d hdr 0x%x level %u lv0_idx %u pms0 0x%08x volt0 %u\n",
+			vclk->name, i, hdr_off, level, lv0_idx, c->orig_pms0, c->orig_volt0);
+	}
+}
+
+static ssize_t oc_fvmap_show(struct oc_cluster *c, char *buf)
+{
+	struct fvmap_header *fh = fvmap_base;
+	void __iomem *sram = sram_fvmap_base;
+	struct cmucal_pll *pll = c->pll;
+	u16 level, members;
+	u32 v;
+	int n = 0, i, j;
+
+	if (c->idx < 0)
+		return -ENODEV;
+
+	fh += c->idx;
+	members = fh->num_of_members;
+	level = min_t(u16, readw(c->pll_hdr + 6), OC_MAX_LEVEL);
+
+	n += scnprintf(buf + n, PAGE_SIZE - n,
+		       "idx %d lv %u mem %u pll %u mux %u div %u o_mem 0x%x o_rv 0x%x o_tbl 0x%x\n",
+		       c->idx, fh->num_of_lv, members, fh->num_of_pll, fh->num_of_mux,
+		       fh->num_of_div, fh->o_members, fh->o_ratevolt, fh->o_tables);
+	n += scnprintf(buf + n, PAGE_SIZE - n, "pll addr 0x%08x o_lock 0x%x level %u shift m%u p%u s%u\n",
+		       readl(c->pll_hdr), readw(c->pll_hdr + 4), level,
+		       pll ? pll->m_shift : 0, pll ? pll->p_shift : 0, pll ? pll->s_shift : 0);
+
+	for (i = 0; i < level; i++) {
+		v = readl(c->pll_hdr + 8 + i * 4);
+		n += scnprintf(buf + n, PAGE_SIZE - n, "pms[%2d] 0x%08x", i, v);
+		if (pll && pll->m_width)
+			n += scnprintf(buf + n, PAGE_SIZE - n, " m %u p %u s %u",
+				       oc_field(v, pll->m_shift, pll->m_width),
+				       oc_field(v, pll->p_shift, pll->p_width),
+				       oc_field(v, pll->s_shift, pll->s_width));
+		n += scnprintf(buf + n, PAGE_SIZE - n, "\n");
+	}
+
+	for (i = 0; i < fh->num_of_lv; i++) {
+		n += scnprintf(buf + n, PAGE_SIZE - n, "lv[%2d] %7u %7u tbl",
+			       i, readl(sram + fh->o_ratevolt + i * 8),
+			       readl(sram + fh->o_ratevolt + i * 8 + 4));
+		for (j = 0; j < members; j++)
+			n += scnprintf(buf + n, PAGE_SIZE - n, " %u",
+				       readb(sram + fh->o_tables + i * members + j));
+		n += scnprintf(buf + n, PAGE_SIZE - n, "\n");
+	}
+
+	return n;
+}
+
+static ssize_t oc_pll_m_show(struct oc_cluster *c, char *buf)
+{
+	struct cmucal_pll *pll = c->pll;
+
+	if (!c->pms0 || !oc_pms_is_stock(c))
+		return -ENODEV;
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n",
+			 oc_field(readl(c->pms0), pll->m_shift, pll->m_width));
+}
+
+static ssize_t oc_pll_m_store(struct oc_cluster *c, const char *buf, size_t count)
+{
+	struct cmucal_pll *pll = c->pll;
+	unsigned int m;
+	u32 v;
+
+	if (!c->pms0 || !oc_pms_is_stock(c))
+		return -ENODEV;
+	if (kstrtouint(buf, 0, &m) || m < OC_PLL_M_STOCK || m > OC_PLL_M_MAX)
+		return -EINVAL;
+
+	mutex_lock(&oc_lock);
+	v = c->orig_pms0 & ~get_mask(pll->m_width, pll->m_shift);
+	v |= m << pll->m_shift;
+	writel(v, c->pms0);
+	mutex_unlock(&oc_lock);
+
+	return count;
+}
+
+static ssize_t oc_pms0_raw_show(struct oc_cluster *c, char *buf)
+{
+	if (!c->pms0)
+		return -ENODEV;
+
+	return scnprintf(buf, PAGE_SIZE, "0x%08x orig 0x%08x\n", readl(c->pms0), c->orig_pms0);
+}
+
+static ssize_t oc_pms0_raw_store(struct oc_cluster *c, const char *buf, size_t count)
+{
+	u32 v;
+
+	if (!c->pms0)
+		return -ENODEV;
+	if (kstrtou32(buf, 0, &v))
+		return -EINVAL;
+
+	mutex_lock(&oc_lock);
+	writel(v, c->pms0);
+	mutex_unlock(&oc_lock);
+
+	return count;
+}
+
+static ssize_t oc_volt0_show(struct oc_cluster *c, char *buf)
+{
+	if (c->idx < 0)
+		return -ENODEV;
+
+	return scnprintf(buf, PAGE_SIZE, "%u orig %u\n",
+			 oc_volt_to_uv(c, readl(c->volt0)), oc_volt_to_uv(c, c->orig_volt0));
+}
+
+static ssize_t oc_volt0_store(struct oc_cluster *c, const char *buf, size_t count)
+{
+	unsigned int uv;
+
+	if (c->idx < 0)
+		return -ENODEV;
+	if (kstrtouint(buf, 0, &uv) || uv < OC_VOLT_MIN_UV || uv > OC_VOLT_MAX_UV)
+		return -EINVAL;
+
+	mutex_lock(&oc_lock);
+	writel(c->volt_steps ? DIV_ROUND_UP(uv, STEP_UV) : uv, c->volt0);
+	mutex_unlock(&oc_lock);
+
+	return count;
+}
+
+#define oc_attr_ro(cl, name)								\
+static ssize_t cpucl##cl##_##name##_show						\
+(struct kobject *kobj, struct kobj_attribute *attr, char *buf)				\
+{											\
+	return oc_##name##_show(&oc_cl[cl], buf);					\
+}											\
+static struct kobj_attribute cpucl##cl##_##name = __ATTR(cpucl##cl##_##name, 0444,	\
+	cpucl##cl##_##name##_show, NULL)
+
+#define oc_attr_rw(cl, name)								\
+static ssize_t cpucl##cl##_##name##_show						\
+(struct kobject *kobj, struct kobj_attribute *attr, char *buf)				\
+{											\
+	return oc_##name##_show(&oc_cl[cl], buf);					\
+}											\
+static ssize_t cpucl##cl##_##name##_store						\
+(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)	\
+{											\
+	return oc_##name##_store(&oc_cl[cl], buf, count);				\
+}											\
+static struct kobj_attribute cpucl##cl##_##name = __ATTR(cpucl##cl##_##name, 0644,	\
+	cpucl##cl##_##name##_show, cpucl##cl##_##name##_store)
+
+oc_attr_ro(0, fvmap);
+oc_attr_ro(1, fvmap);
+oc_attr_rw(0, pll_m);
+oc_attr_rw(1, pll_m);
+oc_attr_rw(0, pms0_raw);
+oc_attr_rw(1, pms0_raw);
+oc_attr_rw(0, volt0);
+oc_attr_rw(1, volt0);
+
+static struct attribute *cpu_oc_attrs[] = {
+	&cpucl0_fvmap.attr,
+	&cpucl1_fvmap.attr,
+	&cpucl0_pll_m.attr,
+	&cpucl1_pll_m.attr,
+	&cpucl0_pms0_raw.attr,
+	&cpucl1_pms0_raw.attr,
+	&cpucl0_volt0.attr,
+	&cpucl1_volt0.attr,
+	NULL,
+};
+
+static const struct attribute_group cpu_oc_group = {
+	.attrs = cpu_oc_attrs,
+};
+
 int fvmap_init(void __iomem *sram_base)
 {
 	void __iomem *map_base;
@@ -361,6 +635,11 @@ int fvmap_init(void __iomem *sram_base)
 
 	if (sysfs_create_group(kobj, &percent_margin_group))
 		pr_err("Fail to create percent_margin group\n");
+
+	cpu_oc_setup(sram_base);
+	kobj = kobject_create_and_add("cpu_oc", kernel_kobj);
+	if (!kobj || sysfs_create_group(kobj, &cpu_oc_group))
+		pr_err("Fail to create cpu_oc group\n");
 
 	return 0;
 }
